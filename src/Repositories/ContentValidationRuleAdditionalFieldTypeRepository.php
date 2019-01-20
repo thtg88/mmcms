936b7b43b42847ac95ad931082137abfdad40786
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-20 21:20:57 +0000

    Added support for content validation rule additional field type model, repository, controller, http requests, and routes

---
+++ src/Repositories/ContentValidationRuleAdditionalFieldTypeRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Thtg88\MmCms\Repositories;
+
+use Thtg88\MmCms\Models\ContentValidationRuleAdditionalFieldType;
+
+/**
+ *
+ */
+class ContentValidationRuleAdditionalFieldTypeRepository extends Repository
+{
+    protected static $model_name = 'name';
+
+    protected static $order_by_columns = [
+        'name' => 'asc',
+    ];
+
+    protected static $search_columns = [
+        'name',
+    ];
+
+    /**
+     * Create a new repository instance.
+     *
+     * @param \Thtg88\MmCms\Models\ContentValidationRuleAdditionalFieldType     $model
+     * @return  void
+     */
+    public function __construct(ContentValidationRuleAdditionalFieldType $model)
+    {
+        $this->model = $model;
+
+        parent::__construct();
+    }
+}
