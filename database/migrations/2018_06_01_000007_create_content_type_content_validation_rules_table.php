01cb2555e6db9693c360473fd832328004bdda2d
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-20 18:14:45 +0000

    Added content model migrations

---
+++ database/migrations/2018_06_01_000007_create_content_type_content_validation_rules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTypeContentValidationRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_type_content_validation_rules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('content_type_id');
+            $table->unsignedInteger('content_validation_rule_id');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->index('content_type_id');
+            $table->index('content_validation_rule_id', 'content_type_validation_rules_rule_id_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_type_content_validation_rules');
+    }
+}
---
+++ database/migrations/2018_06_01_000008_create_content_fields_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('content_model_id');
+            $table->unsignedInteger('content_type_id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->text('helper_text')->nullable();
+            $table->boolean('is_resource_name')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->index('content_model_id');
+            $table->index('content_type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_fields');
+    }
+}
