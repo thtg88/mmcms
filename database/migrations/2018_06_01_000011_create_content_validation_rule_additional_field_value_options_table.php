0d86fd88d96141616f199c29eae7bc26cd058dc9
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-20 20:39:22 +0000

    Added content validation rule additional fields migrations

---
+++ database/migrations/2018_06_01_000011_create_content_validation_rule_additional_field_value_options_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentValidationRuleAdditionalFieldValueOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_validation_rule_additional_field_value_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('content_validation_rule_additional_field_id');
+            $table->string('text');
+            $table->string('value')->nullable();
+            $table->boolean('is_empty_option')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_validation_rule_additional_field_value_options');
+    }
+}
