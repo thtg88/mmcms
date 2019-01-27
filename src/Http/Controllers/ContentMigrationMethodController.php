1f331b0643d79f62db883b047f9d58e0cd9ad138
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-27 10:06:30 +0000

    Added support for content migration method model, repository, controller, http requests, and routes

---
+++ src/Http/Controllers/ContentMigrationMethodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Thtg88\MmCms\Http\Controllers;
+
+// Repositories
+use Thtg88\MmCms\Repositories\ContentMigrationMethodRepository;
+// Requests
+use Thtg88\MmCms\Http\Requests\ContentMigrationMethod\DestroyContentMigrationMethodRequest;
+use Thtg88\MmCms\Http\Requests\ContentMigrationMethod\StoreContentMigrationMethodRequest;
+use Thtg88\MmCms\Http\Requests\ContentMigrationMethod\UpdateContentMigrationMethodRequest;
+
+class ContentMigrationMethodController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @param       \Thtg88\MmCms\Repositories\ContentMigrationMethodRepository        $repository
+     * @return      void
+     */
+    public function __construct(ContentMigrationMethodRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Thtg88\MmCms\Http\Requests\ContentMigrationMethod\StoreContentMigrationMethodRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreContentMigrationMethodRequest $request)
+    {
+        // Get input
+        $input = $request->all();
+
+        // Create
+        $resource = $this->repository->create($input);
+
+        return response()->json([
+            'success' => true,
+            'resource' => $resource,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Thtg88\MmCms\Http\Requests\ContentMigrationMethod\UpdateContentMigrationMethodRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateContentMigrationMethodRequest $request, $id)
+    {
+        // Get input
+        $input = $request->all();
+
+        // Update
+        $resource = $this->repository->update($id, $input);
+
+        // No need to check if found as done by authorization method
+
+        return response()->json([
+            'success' => true,
+            'resource' => $resource,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Thtg88\MmCms\Http\Requests\ContentMigrationMethod\DestroyContentMigrationMethodRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DestroyContentMigrationMethodRequest $request, $id)
+    {
+        // Delete resource
+        $resource = $this->repository->destroy($id);
+
+        return response()->json([
+            'success' => true,
+            'resource' => $resource,
+        ]);
+    }
+}
