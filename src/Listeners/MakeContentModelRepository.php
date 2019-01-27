962609591a1a5e4ea7870960e02a49c575410fe5
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-27 16:53:05 +0000

    Added make content model repository event listener

---
+++ src/Listeners/MakeContentModelRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Thtg88\MmCms\Listeners;
+
+use Thtg88\MmCms\Events\ContentModelStored;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class MakeContentModelRepository
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ContentModelStored  $event
+     * @return void
+     */
+    public function handle(ContentModelStored $event)
+    {
+        $repository_name = studly_case($event->content_model->name).'Repository';
+
+        \Artisan::call('make:repository', [
+            'name' => $repository_name,
+        ]);
+    }
+}
