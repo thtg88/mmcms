22eb2d0ac3f014dd4c405e5cfa88434d2e9d4f3e
Marco Marassi <thtg88@users.noreply.github.com>
2019-01-27 15:49:11 +0000

    Added make content model destroy request event listener

--- src/Listeners/MakeContentModelController.php
+++ src/Listeners/MakeContentModelController.php
@@ -37,19 +37,19 @@ class MakeContentModelController
 
             if(file_exists(app_path('Http/Controllers/'.$controller_name.'.php')))
             {
-                $model_content = file_get_contents(app_path('Http/Controllers/'.$controller_name.'.php'));
+                $file_content = file_get_contents(app_path('Http/Controllers/'.$controller_name.'.php'));
 
-                if($model_content !== false)
+                if($file_content !== false)
                 {
                     $replace_content = $this->getContentModelControllerAdditionalContent($model_name);
 
-                    $model_content = str_replace('//', $replace_content, $model_content);
+                    $file_content = str_replace('//', $replace_content, $file_content);
 
                     $replace_content = $this->getContentModelControllerImports($model_name);
 
-                    $model_content = str_replace('use Illuminate\Http\Request;', $replace_content, $model_content);
+                    $file_content = str_replace('use Illuminate\Http\Request;', $replace_content, $file_content);
 
-                    file_put_contents(app_path('Http/Controllers/'.$controller_name.'.php'), $model_content);
+                    file_put_contents(app_path('Http/Controllers/'.$controller_name.'.php'), $file_content);
                 }
             }
         }
@@ -63,15 +63,15 @@ class MakeContentModelController
      */
     private function getContentModelControllerImports($model_name)
     {
-        $replace_content = '';
-        $replace_content .= "// Repositories\n";
-        $replace_content .= "use App\Repositories\\".$model_name."Repository;\n";
-        $replace_content .= "// Requests\n";
-        $replace_content .= "use App\Http\Requests\\".$model_name."\Destroy".$model_name."Request;\n";
-        $replace_content .= "use App\Http\Requests\\".$model_name."\Store".$model_name."Request;\n";
-        $replace_content .= "use App\Http\Requests\\".$model_name."\Update".$model_name."Request;";
+        $content = '';
+        $content .= "// Repositories\n";
+        $content .= "use App\Repositories\\".$model_name."Repository;\n";
+        $content .= "// Requests\n";
+        $content .= "use App\Http\Requests\\".$model_name."\Destroy".$model_name."Request;\n";
+        $content .= "use App\Http\Requests\\".$model_name."\Store".$model_name."Request;\n";
+        $content .= "use App\Http\Requests\\".$model_name."\Update".$model_name."Request;";
 
-        return $replace_content;
+        return $content;
     }
 
     /**
@@ -82,79 +82,79 @@ class MakeContentModelController
      */
     private function getContentModelControllerAdditionalContent($model_name)
     {
-        $replace_content = '';
-        $replace_content .= "/**\n";
-        $replace_content .= "     * Create a new controller instance.\n";
-        $replace_content .= "     *\n";
-        $replace_content .= "     * @param       \Thtg88\MmCms\Repositories\\".$model_name."Repository        \$repository\n";
-        $replace_content .= "     * @return      void\n";
-        $replace_content .= "     */\n";
-        $replace_content .= "    public function __construct(".$model_name."Repository \$repository)\n";
-        $replace_content .= "    {\n";
-        $replace_content .= "        \$this->repository = \$repository;\n";
-        $replace_content .= "    }\n";
-        $replace_content .= "\n";
-        $replace_content .= "    /**\n";
-        $replace_content .= "     * Store a newly created resource in storage.\n";
-        $replace_content .= "     *\n";
-        $replace_content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Store".$model_name."Request  \$request\n";
-        $replace_content .= "     * @return \Illuminate\Http\Response\n";
-        $replace_content .= "     */\n";
-        $replace_content .= "    public function store(Store".$model_name."Request \$request)\n";
-        $replace_content .= "    {\n";
-        $replace_content .= "        // Get input\n";
-        $replace_content .= "        \$input = \$request->all();\n";
-        $replace_content .= "\n";
-        $replace_content .= "        // Create\n";
-        $replace_content .= "        \$resource = \$this->repository->create(\$input);\n";
-        $replace_content .= "\n";
-        $replace_content .= "        return response()->json([\n";
-        $replace_content .= "            'success' => true,\n";
-        $replace_content .= "            'resource' => \$resource,\n";
-        $replace_content .= "        ]);\n";
-        $replace_content .= "    }\n";
-        $replace_content .= "\n";
-        $replace_content .= "    /**\n";
-        $replace_content .= "     * Update the specified resource in storage.\n";
-        $replace_content .= "     *\n";
-        $replace_content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Update".$model_name."Request  \$request\n";
-        $replace_content .= "     * @param  int  \$id\n";
-        $replace_content .= "     * @return \Illuminate\Http\Response\n";
-        $replace_content .= "     */\n";
-        $replace_content .= "    public function update(Update".$model_name."Request \$request, \$id)\n";
-        $replace_content .= "    {\n";
-        $replace_content .= "        // Get input\n";
-        $replace_content .= "        \$input = \$request->all();\n";
-        $replace_content .= "\n";
-        $replace_content .= "        // Update\n";
-        $replace_content .= "        \$resource = \$this->repository->update(\$id, \$input);\n";
-        $replace_content .= "\n";
-        $replace_content .= "        // No need to check if found as done by authorization method\n";
-        $replace_content .= "\n";
-        $replace_content .= "        return response()->json([\n";
-        $replace_content .= "            'success' => true,\n";
-        $replace_content .= "            'resource' => \$resource,\n";
-        $replace_content .= "        ]);\n";
-        $replace_content .= "    }\n";
-        $replace_content .= "\n";
-        $replace_content .= "    /**\n";
-        $replace_content .= "     * Remove the specified resource from storage.\n";
-        $replace_content .= "     *\n";
-        $replace_content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Destroy".$model_name."Request  \$request\n";
-        $replace_content .= "     * @param  int  \$id\n";
-        $replace_content .= "     * @return \Illuminate\Http\Response\n";
-        $replace_content .= "     */\n";
-        $replace_content .= "    public function destroy(Destroy".$model_name."Request \$request, \$id)\n";
-        $replace_content .= "    {\n";
-        $replace_content .= "        // Delete resource\n";
-        $replace_content .= "        \$resource = \$this->repository->destroy(\$id);\n";
-        $replace_content .= "\n";
-        $replace_content .= "        return response()->json([\n";
-        $replace_content .= "            'success' => true,\n";
-        $replace_content .= "            'resource' => \$resource,\n";
-        $replace_content .= "        ]);\n";
-        $replace_content .= "    }";
+        $content = '';
+        $content .= "/**\n";
+        $content .= "     * Create a new controller instance.\n";
+        $content .= "     *\n";
+        $content .= "     * @param       \Thtg88\MmCms\Repositories\\".$model_name."Repository        \$repository\n";
+        $content .= "     * @return      void\n";
+        $content .= "     */\n";
+        $content .= "    public function __construct(".$model_name."Repository \$repository)\n";
+        $content .= "    {\n";
+        $content .= "        \$this->repository = \$repository;\n";
+        $content .= "    }\n";
+        $content .= "\n";
+        $content .= "    /**\n";
+        $content .= "     * Store a newly created resource in storage.\n";
+        $content .= "     *\n";
+        $content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Store".$model_name."Request  \$request\n";
+        $content .= "     * @return \Illuminate\Http\Response\n";
+        $content .= "     */\n";
+        $content .= "    public function store(Store".$model_name."Request \$request)\n";
+        $content .= "    {\n";
+        $content .= "        // Get input\n";
+        $content .= "        \$input = \$request->all();\n";
+        $content .= "\n";
+        $content .= "        // Create\n";
+        $content .= "        \$resource = \$this->repository->create(\$input);\n";
+        $content .= "\n";
+        $content .= "        return response()->json([\n";
+        $content .= "            'success' => true,\n";
+        $content .= "            'resource' => \$resource,\n";
+        $content .= "        ]);\n";
+        $content .= "    }\n";
+        $content .= "\n";
+        $content .= "    /**\n";
+        $content .= "     * Update the specified resource in storage.\n";
+        $content .= "     *\n";
+        $content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Update".$model_name."Request  \$request\n";
+        $content .= "     * @param  int  \$id\n";
+        $content .= "     * @return \Illuminate\Http\Response\n";
+        $content .= "     */\n";
+        $content .= "    public function update(Update".$model_name."Request \$request, \$id)\n";
+        $content .= "    {\n";
+        $content .= "        // Get input\n";
+        $content .= "        \$input = \$request->all();\n";
+        $content .= "\n";
+        $content .= "        // Update\n";
+        $content .= "        \$resource = \$this->repository->update(\$id, \$input);\n";
+        $content .= "\n";
+        $content .= "        // No need to check if found as done by authorization method\n";
+        $content .= "\n";
+        $content .= "        return response()->json([\n";
+        $content .= "            'success' => true,\n";
+        $content .= "            'resource' => \$resource,\n";
+        $content .= "        ]);\n";
+        $content .= "    }\n";
+        $content .= "\n";
+        $content .= "    /**\n";
+        $content .= "     * Remove the specified resource from storage.\n";
+        $content .= "     *\n";
+        $content .= "     * @param  \Thtg88\MmCms\Http\Requests\\".$model_name."\Destroy".$model_name."Request  \$request\n";
+        $content .= "     * @param  int  \$id\n";
+        $content .= "     * @return \Illuminate\Http\Response\n";
+        $content .= "     */\n";
+        $content .= "    public function destroy(Destroy".$model_name."Request \$request, \$id)\n";
+        $content .= "    {\n";
+        $content .= "        // Delete resource\n";
+        $content .= "        \$resource = \$this->repository->destroy(\$id);\n";
+        $content .= "\n";
+        $content .= "        return response()->json([\n";
+        $content .= "            'success' => true,\n";
+        $content .= "            'resource' => \$resource,\n";
+        $content .= "        ]);\n";
+        $content .= "    }";
 
-        return $replace_content;
+        return $content;
     }
 }
