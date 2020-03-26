5d2719f48120dd0bf30883e98017a79a5223742e
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-26 15:57:43 +0000

    Renamed index to paginate in Service layer

--- src/Services/ResourceService.php
+++ src/Services/ResourceService.php
@@ -3,7 +3,7 @@
 namespace Thtg88\MmCms\Services;
 
 use Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface;
-use Thtg88\MmCms\Http\Requests\Contracts\IndexRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\PaginateRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\RestoreRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\SearchRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface;
@@ -72,10 +72,10 @@ class ResourceService implements ResourceServiceInterface
     /**
      * Return all the model instances.
      *
-     * @param \Thtg88\MmCms\Http\Requests\Contracts\IndexRequestInterface $request
+     * @param \Thtg88\MmCms\Http\Requests\Contracts\PaginateRequestInterface $request
      * @return \Illuminate\Support\Collection
      */
-    public function index(IndexRequestInterface $request)
+    public function paginate(PaginateRequestInterface $request)
     {
         // Get input
         $input = $request->only([
