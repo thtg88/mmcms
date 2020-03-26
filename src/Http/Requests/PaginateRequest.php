435b6205c2c282cea2d26655d76623e16fb8509d
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-26 15:16:09 +0000

    Added default rules to index and paginate requests

--- src/Http/Requests/PaginateRequest.php
+++ src/Http/Requests/PaginateRequest.php
@@ -22,8 +22,18 @@ class PaginateRequest extends Request
     public function rules()
     {
         return [
+            'filters' => 'nullable|array',
+            'filters.*.name' => 'required|string|min:2|max:255',
+            'filters.*.operator' => 'required|string|in:=,<>,>,<,<=,>=,like,in,between',
+            'filters.*.relationship-field' => 'nullable|string',
+            'filters.*.target_type' => 'nullable|string',
+            'filters.*.value' => 'nullable|string',
             'page' => 'nullable|integer|min:1',
             'page_size' => 'nullable|integer|min:2',
+            'q' => 'nullable|string|max:255',
+            'recovery' => 'nullable|boolean',
+            'sort_name' => 'required_with:sort_direction|string|min:2|max:255',
+            'sort_direction' => 'required_with:sort_name|string|in:asc,desc',
         ];
     }
 }
