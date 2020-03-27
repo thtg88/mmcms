3307469ba516cfe5452dec6f3e86885869fb1bb8
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-27 12:18:37 +0000

    Refactored ImageController to service pattern

--- src/Http/Requests/Image/StoreRequest.php
+++ src/Http/Requests/Image/StoreRequest.php
@@ -4,10 +4,10 @@ namespace Thtg88\MmCms\Http\Requests\Image;
 
 use Illuminate\Validation\Rule;
 use Thtg88\MmCms\Helpers\DatabaseHelper;
-use Thtg88\MmCms\Http\Requests\UpdateRequest;
+use Thtg88\MmCms\Http\Requests\StoreRequest;
 use Thtg88\MmCms\Repositories\ImageRepository;
 
-class UpdateImageRequest extends UpdateRequest
+class StoreRequest extends StoreRequest
 {
     /**
      * Create a new request instance.
@@ -29,7 +29,7 @@ class UpdateImageRequest extends UpdateRequest
      */
     public function authorize()
     {
-        return $this->authorizeResourceExist();
+        return true;
     }
 
     /**
@@ -42,29 +42,12 @@ class UpdateImageRequest extends UpdateRequest
         // Get input
         $input = $this->all();
 
-        // Get resource id
-        $resource_id = $this->route('id');
-
-        // Get resource
-        $resource = $this->repository->find($resource_id, false, true);
-
         // Get all tables
         $table_names = $this->database_helper->getTableNames();
 
-        // We define the real table name in order to check if the target exists
-        $real_table_name = null;
-
-        // First we check if the provided input contains a table name
-        if (array_key_exists('target_table', $input) && in_array($input['target_table'], $table_names, true)) {
-            // If so, this is the table name we are going to use for our validation
-            $real_table_name = $input['target_table'];
-        } else {
-            // Otherwise use the existing table name
-            $real_table_name = $resource->target_table;
-        }
-
         $all_rules = [
             'caption' => 'nullable|string',
+            'data' => 'required_without:url|file|mimes:jpg,jpeg,png,gif',
             'name' => 'nullable|string|max:255',
             'target_id' => [
                 'required',
@@ -76,25 +59,16 @@ class UpdateImageRequest extends UpdateRequest
                 'in:'.implode(',', $table_names),
             ],
             'title' => 'nullable|string|max:255',
+            'url' => 'required_without:data|string|max:2000',
         ];
 
-        // Get necessary rules based on input (same keys basically)
-        $rules = array_intersect_key($all_rules, $input);
-
-        if (
-            in_array($real_table_name, $table_names, true)
-            && (
-                array_key_exists('target_id', $input)
-                || array_key_exists('target_table', $input)
-            )
-        ) {
-            // If it's in the list of tables, we want to check it's a valid id in that table
-            $rules['target_id'][] = Rule::exists($real_table_name, 'id')
+        if (array_key_exists('target_table', $input) && in_array($input['target_table'], $table_names, true)) {
+            $all_rules['target_id'][] = Rule::exists($input['target_table'], 'id')
                 ->where(function ($query) {
                     $query->whereNull('deleted_at');
                 });
         }
 
-        return $rules;
+        return $all_rules;
     }
 }
---
+++ src/Services/ImageService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Thtg88\MmCms\Services;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Thtg88\MmCms\Helpers\FileHelper;
+use Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface;
+use Thtg88\MmCms\Repositories\ImageRepository;
+
+class ImageService extends ResourceService
+{
+    /**
+     * Create a new service instance.
+     *
+     * @param \Thtg88\MmCms\Repositories\ImageRepository $repository
+     * @param \Thtg88\MmCms\Helpers\FileHelper $file_helper
+     * @return void
+     */
+    public function __construct(
+        ImageRepository $repository,
+        FileHelper $file_helper
+    ) {
+        $this->repository = $repository;
+        $this->file_helper = $file_helper;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface $request
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function store(StoreRequestInterface $request)
+    {
+        $data = $request->validated();
+
+        if ($request->hasFile('data') && $request->file('data')->isValid()) {
+            // Get path from uploaded file
+            $data['url'] = $this->file_helper
+                ->store($data['data'], $this->repository);
+
+            unset($data['data']);
+        }
+
+        $resource = $this->repository->create($data);
+
+        return $resource;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface $request
+     * @param int $id
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function destroy(DestroyRequestInterface $request, $id)
+    {
+        $resource = $this->repository->destroy($id);
+
+        if (Str::startsWith($resource->url, '/storage/userfiles')) {
+            $path = storage_path(
+                'app/public'.substr($resource->url, strlen('/storage'))
+            );
+
+            if (is_file($path) && is_readable($path)) {
+                // remove from FS
+                unlink($path);
+            }
+        }
+
+        return $resource;
+    }
+}
