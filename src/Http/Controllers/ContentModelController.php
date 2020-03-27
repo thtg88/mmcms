5fd8aea4f51fa393df01b35ad473941266e57c00
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-27 11:40:02 +0000

    Replaced ContentModelController with ContentModelService

--- src/Http/Controllers/ContentModelController.php
+++ src/Http/Controllers/ContentModelController.php
@@ -2,137 +2,37 @@
 
 namespace Thtg88\MmCms\Http\Controllers;
 
-use Thtg88\MmCms\Events\ContentModelStored;
-use Thtg88\MmCms\Repositories\ContentModelRepository;
-use Thtg88\MmCms\Http\Requests\ContentModel\DestroyContentModelRequest;
-use Thtg88\MmCms\Http\Requests\ContentModel\StoreContentModelRequest;
-use Thtg88\MmCms\Http\Requests\ContentModel\UpdateContentModelRequest;
+use Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\UpdateRequestInterface;
+use Thtg88\MmCms\Http\Requests\ContentModel\DestroyRequest;
+use Thtg88\MmCms\Http\Requests\ContentModel\StoreRequest;
+use Thtg88\MmCms\Http\Requests\ContentModel\UpdateRequest;
+use Thtg88\MmCms\Services\ContentModelService;
 
 class ContentModelController extends Controller
 {
     /**
+     * The controller-specific bindings.
+     *
+     * @var string[]|callable[]
+     */
+    protected $bindings = [
+        DestroyRequestInterface::class => DestroyRequest::class,
+        StoreRequestInterface::class => StoreRequest::class,
+        UpdateRequestInterface::class => UpdateRequest::class,
+    ];
+
+    /**
      * Create a new controller instance.
      *
-     * @param \Thtg88\MmCms\Repositories\ContentModelRepository $repository
+     * @param \App\Services\ContentModelService $service
      * @return void
      */
-    public function __construct(ContentModelRepository $repository)
+    public function __construct(ContentModelService $service)
     {
-        $this->repository = $repository;
-    }
+        $this->service = $service;
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Thtg88\MmCms\Http\Requests\ContentModel\StoreContentModelRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreContentModelRequest $request)
-    {
-        // Get input
-        $input = $request->all();
-
-        if (!array_key_exists('base_route_name', $input) || empty($input['base_route_name'])) {
-            // If base route name is not provided we build it
-            // We first get all the lowercase words
-            // We pluralize the last
-            // And finally we separate them by hyphens instead of spaces
-
-            // Get words
-            $words = explode(' ', strtolower($input['name']));
-
-            $input['base_route_name'] = implode(
-                '-',
-                array_map(
-                    function ($word, $idx) use ($words) {
-                        return $idx === count($words) - 1
-                            ? str_plural($word)
-                            : $word;
-                    },
-                    $words,
-                    array_keys($words)
-                )
-            );
-        }
-
-        if (!array_key_exists('model_name', $input) || empty($input['model_name'])) {
-            // If model name is not provided we build it
-            // By studly casing the name
-            $input['model_name'] = studly_case($input['name']);
-        }
-
-        if (!array_key_exists('table_name', $input) || empty($input['table_name'])) {
-            // If table name is not provided we build it
-            // We first get all the lowercase words
-            // We pluralize the last
-            // And finally we separate them by underscores instead of spaces
-
-            // Get words
-            $words = explode(' ', strtolower($input['name']));
-
-            $input['table_name'] = implode(
-                '_',
-                array_map(
-                    function ($word, $idx) use ($words) {
-                        return $idx === count($words) - 1
-                            ? str_plural($word)
-                            : $word;
-                    },
-                    $words,
-                    array_keys($words)
-                )
-            );
-        }
-
-        // Create
-        $resource = $this->repository->create($input);
-
-        event(new ContentModelStored($resource));
-
-        return response()->json([
-            'success' => true,
-            'resource' => $resource,
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Thtg88\MmCms\Http\Requests\ContentModel\UpdateContentModelRequest  $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateContentModelRequest $request, $id)
-    {
-        // Get input
-        $input = $request->all();
-
-        // Update
-        $resource = $this->repository->update($id, $input);
-
-        // No need to check if found as done by authorization method
-
-        return response()->json([
-            'success' => true,
-            'resource' => $resource,
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \Thtg88\MmCms\Http\Requests\ContentModel\DestroyContentModelRequest  $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(DestroyContentModelRequest $request, $id)
-    {
-        // Delete resource
-        $resource = $this->repository->destroy($id);
-
-        return response()->json([
-            'success' => true,
-            'resource' => $resource,
-        ]);
+        parent::__construct();
     }
 }
