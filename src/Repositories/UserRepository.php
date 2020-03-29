ead8375ab56fce0fea0e65668e35ba857b294170
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-29 11:25:20 +0100

    Refactored UserRepository

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -2,26 +2,44 @@
 
 namespace Thtg88\MmCms\Repositories;
 
-use Carbon\Carbon;
 use Thtg88\MmCms\Models\User;
 
-/**
-*
-*/
 class UserRepository extends Repository
 {
+    /**
+     * The column to treat as the model name.
+     *
+     * @var string
+     */
     protected static $model_name = 'email';
 
+    /**
+     * The columns to which perform the sorting on.
+     *
+     * @var array
+     */
     protected static $order_by_columns = [
         'name' => 'asc',
         'email' => 'asc',
         'id' => 'asc',
     ];
 
+    /**
+     * The columns to which perform the search on.
+     *
+     * @var array
+     */
     protected static $search_columns = [
         'email',
         'name',
     ];
+
+    /**
+     * The columns to which perform filtering on.
+     *
+     * @var array
+     */
+    protected static $filter_columns = [];
 
     /**
      * Create a new repository instance.
