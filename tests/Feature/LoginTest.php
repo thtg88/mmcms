bf54c7d7405ab45c9522e026759c467e1856c7df
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-29 21:33:58 +0100

    Added login wrong credentials error test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,6 +2,8 @@
 
 namespace Thtg88\MmCms\Tests\Feature;
 
+use Illuminate\Support\Str;
+use Thtg88\MmCms\Models\User;
 use Thtg88\MmCms\Tests\Feature\TestCase;
 
 class LoginTest extends TestCase
@@ -23,6 +25,50 @@ class LoginTest extends TestCase
                     'email' => ['The email field is required.'],
                     'password' => ['The password field is required.'],
                 ]
+            ]);
+    }
+
+    /** @test */
+    public function wrong_credentials_error(): void
+    {
+        // Create test user
+        $password = 'password';
+        $model = factory(User::class)->create([
+            'password' => $password,
+        ]);
+
+        // Test wrong email and password
+        $response = $this->json('post', $this->url, [
+            'email' => $this->faker->email(),
+            'password' => 'wrong-password',
+        ]);
+        $response->assertStatus(422)
+            ->assertJson([
+                'errors' => [
+                    'email' => ['The selected email is invalid.'],
+                ],
+            ]);
+
+        // Test wrong password
+        $response = $this->json('post', $this->url, [
+            'email' => $model->email,
+            'password' => 'wrong-password',
+        ]);
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'The user credentials were incorrect.',
+            ]);
+
+        // Test wrong email
+        $response = $this->json('post', $this->url, [
+            'email' => $this->faker->email(),
+            'password' => $password,
+        ]);
+        $response->assertStatus(422)
+            ->assertJson([
+                'errors' => [
+                    'email' => ['The selected email is invalid.'],
+                ],
             ]);
     }
 
