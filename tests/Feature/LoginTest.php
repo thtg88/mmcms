86f601512d6ea000e11877d927d5617f26030dea
Marco Marassi <thtg88@users.noreply.github.com>
2020-03-30 17:19:07 +0100

    Mock passport authentication in login test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,20 +2,22 @@
 
 namespace Thtg88\MmCms\Tests\Feature;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
+use Laravel\Passport\Passport;
+use Mockery as m;
 use Thtg88\MmCms\Models\User;
+use Thtg88\MmCms\Repositories\UserRepository;
 use Thtg88\MmCms\Tests\Feature\TestCase;
 
 class LoginTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->url = route('mmcms.auth.login');
-    }
-
     /** @test */
     public function empty_payload_has_required_validation_errors(): void
     {
@@ -83,25 +85,84 @@ class LoginTest extends TestCase
         ]);
 
         // Test successful login
-        $response = $this->json('post', $this->url, [
-            'email' => $model->email,
-            'password' => $password,
-        ]);
-        $response//->assertStatus(200)
-            ->assertJson(['foo' => 'bar']);
+        $response = $this->mockOauthHttpClient($model->email)
+            ->json('post', $this->url, [
+                'email' => $model->email,
+                'password' => $password,
+            ]);
+        $response->assertStatus(200)
+            ->assertJson([
+                'token_type' => 'Bearer',
+                'expires_in' => 31536000,
+                'access_token' => 'access-token',
+                'refresh_token' => 'refresh-token',
+            ]);
         $this->assertAuthenticated();
         $this->assertAuthenticatedAs($model);
 
-        Auth::logout();
+        $this->passportAuthLogout();
 
         // Test successful login with wrong email casing
-        $response = $this->json('post', $this->url, [
-            'email' => strtoupper($model->email),
-            'password' => $password,
-        ]);
+        $response = $this->mockOauthHttpClient(strtoupper($model->email))
+            ->json('post', $this->url, [
+                'email' => strtoupper($model->email),
+                'password' => $password,
+            ]);
         $response->assertStatus(200)
-            ->assertJson(['foo' => 'bar']);
+            ->assertJson([
+                'token_type' => 'Bearer',
+                'expires_in' => 31536000,
+                'access_token' => 'access-token',
+                'refresh_token' => 'refresh-token',
+            ]);
         $this->assertAuthenticated();
         $this->assertAuthenticatedAs($model);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->url = route('mmcms.auth.login');
+    }
+
+    protected function tearDown(): void
+    {
+        m::close();
+
+        parent::tearDown();
+    }
+
+    protected function mockOauthHttpClient($email): self
+    {
+        $user = app()->make(UserRepository::class)
+            ->findByModelName(strtolower($email));
+
+        if ($user === null) {
+            throw new InvalidArgumentException(
+                'The email provided does not have a corresponding user'.
+                ' in the database. Please create one before continuing'
+            );
+        }
+
+        Passport::actingAs($user);
+
+        $app = Container::getInstance();
+
+        // Mock OAuth Passport HTTP Client
+        $mock = new MockHandler([
+            new GuzzleResponse(200, ['Content-Type' => 'application/json'], json_encode([
+                'token_type' => 'Bearer',
+                'expires_in' => 31536000,
+                'access_token' => 'access-token',
+                'refresh_token' => 'refresh-token',
+            ]))
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $app->instance('OauthHttpClient', $client);
+
+        return $this;
     }
 }
