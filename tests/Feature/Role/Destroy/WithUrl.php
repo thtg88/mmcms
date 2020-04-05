040b4465ab9c6374e2d42b3c794cfd921712efae
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-05 11:33:27 +0100

    Add role tests

---
+++ tests/Feature/Role/Destroy/WithUrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\Role\Destroy;
+
+trait WithUrl
+{
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string
+    {
+        return route('mmcms.roles.destroy', ['id' => $parameters[0]]);
+    }
+}
