69a9a0bb227245c962101f4cbf8978958bbd2621
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-05 11:31:37 +0100

    Add generic test contracts

---
+++ tests/Contracts/UnarchiveTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Contracts;
+
+interface UnarchiveTest
+{
+    /**
+     * Test an empty payload has required validation errors.
+     *
+     * @return void
+     */
+    public function testNonExistingModelAuthorizationErrors();
+
+    /**
+     * Test an empty payload does not have any validation errors.
+     *
+     * @return void
+     */
+    public function testEmptyPayloadHasNoErrors();
+
+    /**
+     * Test successful update.
+     *
+     * @return void
+     */
+    public function testSuccessfulUnarchive();
+
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string;
+}
