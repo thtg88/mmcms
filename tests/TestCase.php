e38f48f3c1996828624b6d663f6d5bcc977858b3
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-05 11:06:17 +0100

    Make sure tests base classes are abstract

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Thtg88\MmCms\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-class TestCase extends OrchestraTestCase
+abstract class TestCase extends OrchestraTestCase
 {
     //
 }
