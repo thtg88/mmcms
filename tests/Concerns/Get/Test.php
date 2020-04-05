d221f5d7034b603917256cbe8448cac9ca77ee58
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-05 17:18:36 +0100

    Tests concerns naming and typing improvements

--- tests/Concerns/Get/Test.php
+++ tests/Concerns/Get/Test.php
@@ -5,12 +5,11 @@ namespace Thtg88\MmCms\Tests\Concerns\Get;
 trait Test
 {
     /**
-     * Test successful get request.
-     *
      * @return void
      * @group get-tests
+     * @test
      */
-    public function testSuccessfulGet()
+    public function successful_get(): void
     {
         $response = $this->json('get', $this->getRoute());
         $response->assertStatus(200);
