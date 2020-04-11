9f03b582369a425ce6d5185784f77f51629b494b
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-11 16:16:11 +0100

    Define default authorization role for policies

--- src/Policies/RolePolicy.php
+++ src/Policies/RolePolicy.php
@@ -2,7 +2,17 @@
 
 namespace Thtg88\MmCms\Policies;
 
+use Illuminate\Support\Facades\Config;
+
 class RolePolicy extends Policy
 {
-    //
+    /**
+     * Get the default authorized role for the policy.
+     *
+     * @return string
+     */
+    public function getDefaultAuthorizationRole(): string
+    {
+        return Config::get('mmcms.roles.names.developer');
+    }
 }
