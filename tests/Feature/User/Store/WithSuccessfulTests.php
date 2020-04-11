c5f606c255f74e397afe179f4867890001e03ab0
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-11 17:57:55 +0100

    Sort imports alphabetically in user WithSuccessfulTests concern

--- tests/Feature/User/Store/WithSuccessfulTests.php
+++ tests/Feature/User/Store/WithSuccessfulTests.php
@@ -2,10 +2,11 @@
 
 namespace Thtg88\MmCms\Tests\Feature\User\Store;
 
-use Thtg88\MmCms\Models\Role;
-use Thtg88\MmCms\Models\User;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
+use Thtg88\MmCms\Models\Role;
+use Thtg88\MmCms\Models\User;
+use Thtg88\MmCms\Repositories\RoleRepository;
 
 trait WithSuccessfulTests
 {
