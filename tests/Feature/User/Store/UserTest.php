a981e90f5538f8fd3938ca6917f7c7ceb61a9703
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-11 16:59:45 +0100

    Added User Store UserTest

---
+++ tests/Feature/User/Store/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\User\Store;
+
+use Thtg88\MmCms\Tests\Concerns\Store\Unauthorized\ActingAsUserTest;
+use Thtg88\MmCms\Tests\Feature\User\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsUserTest;
+}
