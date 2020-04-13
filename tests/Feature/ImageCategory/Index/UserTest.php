e1430f92e5161bc3262c64c9b6f16ff7b746af43
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-13 16:35:12 +0100

    Added image category index tests

---
+++ tests/Feature/ImageCategory/Index/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\ImageCategory\Index;
+
+use Thtg88\MmCms\Tests\Concerns\Get\Unauthorized\ActingAsUserTest;
+use Thtg88\MmCms\Tests\Feature\ImageCategory\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsUserTest;
+}
