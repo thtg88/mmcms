f5d7cc334cf1059dd57110d3da21c35eb7b8584a
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-13 16:36:52 +0100

    Added image category paginate tests

---
+++ tests/Feature/ImageCategory/Paginate/DevTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\ImageCategory\Paginate;
+
+use Thtg88\MmCms\Tests\Concerns\Get\ActingAsDevTest;
+use Thtg88\MmCms\Tests\Feature\ImageCategory\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class DevTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsDevTest;
+}
