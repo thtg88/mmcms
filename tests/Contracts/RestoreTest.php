1d26b17466a264a5a959f82fc78f734ab3c77eb7
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-13 15:39:56 +0100

    Added restore test contract

---
+++ tests/Contracts/RestoreTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Contracts;
+
+interface RestoreTest
+{
+    /**
+     * Test an empty payload has required validation errors.
+     *
+     * @return void
+     */
+    public function non_existing_model_authorization_errors();
+
+    /**
+     * Test successful restore.
+     *
+     * @return void
+     */
+    public function successful_restore();
+
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string;
+}
