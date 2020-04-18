0bff545a9d18646049698a6c3051ca9ac39dd7d2
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-18 14:03:46 +0100

    Role Show tests

---
+++ tests/Feature/Role/Show/WithUrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\Role\Show;
+
+trait WithUrl
+{
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string
+    {
+        return route('mmcms.roles.show', ['id' => $parameters[0]]);
+    }
+}
