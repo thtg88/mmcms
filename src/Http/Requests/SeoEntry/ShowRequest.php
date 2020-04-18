8944a01aaec31d76637fc540b142e4b4637735d4
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-18 14:04:34 +0100

    Hook SeoEntry ShowRequest

---
+++ src/Http/Requests/SeoEntry/ShowRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Thtg88\MmCms\Http\Requests\SeoEntry;
+
+use Thtg88\MmCms\Http\Requests\ShowRequest as BaseShowRequest;
+use Thtg88\MmCms\Repositories\SeoEntryRepository;
+
+class ShowRequest extends BaseShowRequest
+{
+    /**
+     * Create a new request instance.
+     *
+     * @param \Thtg88\MmCms\Repositories\SeoEntryRepository $repository
+     * @return void
+     */
+    public function __construct(SeoEntryRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+}
