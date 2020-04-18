4541f563d8e24b60e97817ebfa97bbcfcdd64714
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-18 14:00:17 +0100

    Added user show tests

---
+++ tests/Feature/User/Show/WithUrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\User\Show;
+
+trait WithUrl
+{
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string
+    {
+        return route('mmcms.users.show', ['id' => $parameters[0]]);
+    }
+}
