13611e825adeb9139e78c7fc5e0f96c9c08b887e
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-18 13:59:39 +0100

    Hook User ShowRequest

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -5,12 +5,14 @@ namespace Thtg88\MmCms\Http\Controllers;
 use Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\IndexRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\PaginateRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\ShowRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\RestoreRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\UpdateRequestInterface;
 use Thtg88\MmCms\Http\Requests\User\DestroyRequest;
 use Thtg88\MmCms\Http\Requests\User\IndexRequest;
 use Thtg88\MmCms\Http\Requests\User\PaginateRequest;
+use Thtg88\MmCms\Http\Requests\User\ShowRequest;
 use Thtg88\MmCms\Http\Requests\User\StoreRequest;
 use Thtg88\MmCms\Http\Requests\User\RestoreRequest;
 use Thtg88\MmCms\Http\Requests\User\UpdateRequest;
@@ -27,6 +29,7 @@ class UserController extends Controller
         DestroyRequestInterface::class => DestroyRequest::class,
         IndexRequestInterface::class => IndexRequest::class,
         PaginateRequestInterface::class => PaginateRequest::class,
+        ShowRequestInterface::class => ShowRequest::class,
         StoreRequestInterface::class => StoreRequest::class,
         RestoreRequestInterface::class => RestoreRequest::class,
         UpdateRequestInterface::class => UpdateRequest::class,
