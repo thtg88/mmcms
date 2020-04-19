7ea4e222f46e6216637e5a85e8b618ef26f92b8d
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-19 18:49:58 +0100

    added content type store and update tests

---
+++ tests/Feature/ContentType/Store/AdminTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\ContentType\Store;
+
+use Thtg88\MmCms\Tests\Feature\ContentType\WithModelData;
+use Thtg88\MmCms\Tests\Feature\Concerns\Store\Unauthorized\ActingAsAdminTest;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class AdminTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsAdminTest;
+}
