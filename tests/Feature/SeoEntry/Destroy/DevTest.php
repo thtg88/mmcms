6fa28acbda8e4a055aaba9d1881531d5a56fd345
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-19 13:12:08 +0100

    Added SEO entry destroy

---
+++ tests/Feature/SeoEntry/Destroy/DevTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Destroy;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Destroy\ActingAsDevTest;
+use Thtg88\MmCms\Tests\Feature\Contracts\DestroyTest as DestroyTestContract;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class DevTest extends TestCase implements DestroyTestContract
+{
+    use WithModelData, WithUrl, ActingAsDevTest;
+}
---
+++ tests/Feature/SeoEntry/Destroy/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Destroy;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Destroy\Unauthorized\ActingAsUserTest;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsUserTest;
+}
