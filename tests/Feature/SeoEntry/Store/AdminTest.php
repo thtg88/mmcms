e539ac84f4ba490669ecbd327de0c4d37f9cc00b
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-19 14:00:42 +0100

    Added SeoEntry store tests

---
+++ tests/Feature/SeoEntry/Store/AdminTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Store;
+
+use Thtg88\MmCms\Tests\Feature\Contracts\StoreTest as StoreTestContract;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class AdminTest extends TestCase implements StoreTestContract
+{
+    use WithModelData, WithUrl, WithSuccessfulTests;
+
+    /**
+     * Return the factory state that represent the user role
+     * acting for these tests.
+     *
+     * @return string
+     */
+    protected function getUserRoleFactoryStateName(): string
+    {
+        return 'admin';
+    }
+}
