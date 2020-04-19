ee0f91ab0c1b7efbdf3b97dc68333e2e0f424851
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-19 13:13:09 +0100

    Added SEO entry show tests

---
+++ tests/Feature/SeoEntry/Show/AdminTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Show;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Get\Model\ActingAsAdminTest;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class AdminTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsAdminTest;
+}
---
+++ tests/Feature/SeoEntry/Show/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Show;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Get\Model\ActingAsUserTest;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class UserTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsUserTest;
+}
