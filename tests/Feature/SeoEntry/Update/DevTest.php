692db9a911eedd57c89c17b29aab86b28b6ba410
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-19 17:35:26 +0100

    Added SeoEntry update tests

---
+++ tests/Feature/SeoEntry/Update/DevTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\SeoEntry\Update;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Update\ActingAsDevTest;
+use Thtg88\MmCms\Tests\Feature\Contracts\UpdateTest as UpdateTestContract;
+use Thtg88\MmCms\Tests\Feature\SeoEntry\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class DevTest extends TestCase implements UpdateTestContract
+{
+    use WithModelData, WithUrl, ActingAsDevTest, WithSuccessfulTests;
+
+    /**
+     * Return the factory state that represent the user role
+     * acting for these tests.
+     *
+     * @return string
+     */
+    protected function getUserRoleFactoryStateName(): string
+    {
+        return 'dev';
+    }
+}
