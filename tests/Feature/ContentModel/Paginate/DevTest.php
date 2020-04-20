c0b9c8a32f34b82a21bc1d38226d5c3fe1cb171f
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-20 22:16:12 +0100

    Added content model paginate tests

---
+++ tests/Feature/ContentModel/Paginate/DevTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\ContentModel\Paginate;
+
+use Thtg88\MmCms\Tests\Feature\Concerns\Get\ActingAsDevTest;
+use Thtg88\MmCms\Tests\Feature\ContentModel\WithModelData;
+use Thtg88\MmCms\Tests\Feature\TestCase;
+
+class DevTest extends TestCase
+{
+    use WithModelData, WithUrl, ActingAsDevTest;
+}
