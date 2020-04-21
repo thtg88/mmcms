cb5df3c2d5a2c601e3e9d5710b3a487f5e326101
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-21 21:19:46 +0100

    Refactored make content model store, destroy, and update request event listeners to use existing scaffold commands

--- src/Listeners/MakeContentModelUpdateRequest.php
+++ src/Listeners/MakeContentModelUpdateRequest.php
@@ -10,16 +10,6 @@ use Thtg88\MmCms\Events\ContentModelStored;
 class MakeContentModelUpdateRequest
 {
     /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      *
      * @param \Thtg88\MmCms\Events\ContentModelStored $event
@@ -28,18 +18,10 @@ class MakeContentModelUpdateRequest
     public function handle(ContentModelStored $event): void
     {
         $model_name = Str::studly($event->content_model->name);
-        $request_name = 'Update'.$model_name.'Request';
-
-        if (class_exists($request_name)) {
-            return;
-        }
-
-        Artisan::call('make:request', [
-            'name' => $request_name,
-        ]);
+        $request_name = $model_name.DIRECTORY_SEPARATOR.'UpdateRequest';
 
         if (
-            ! file_exists(
+            file_exists(
                 Container::getInstance()
                     ->path('Http/Requests/'.$request_name.'.php')
             )
@@ -47,95 +29,9 @@ class MakeContentModelUpdateRequest
             return;
         }
 
-        $file_content = file_get_contents(Container::getInstance()->path('Http/Requests/'.$request_name.'.php'));
-
-        if ($file_content === false) {
-            return;
-        }
-
-        $replace_content = $this->getContentModelRequestAuthorizeMethodContent();
-
-        $file_content = str_replace(
-            'return false;',
-            $replace_content,
-            $file_content
-        );
-
-        $replace_content = $this->getContentModelRequestConstructor(
-            $model_name
-        );
-
-        $file_content = str_replace(
-            'class '.$request_name.' extends FormRequest'.PHP_EOL.'{'.PHP_EOL,
-            $replace_content,
-            $file_content
-        );
-
-        $replace_content = $this->getContentModelRequestImports($model_name);
-
-        $file_content = str_replace(
-            'use Illuminate\Foundation\Http\FormRequest;',
-            $replace_content,
-            $file_content
-        );
-
-        file_put_contents(
-            Container::getInstance()
-                ->path('Http/Requests/'.$request_name.'.php'),
-            $file_content
-        );
-    }
-
-    /**
-     * Returns imports for the request from a given model name.
-     *
-     * @param string $model_name
-     * @return string
-     */
-    private function getContentModelRequestImports(string $model_name): string
-    {
-        $content = '';
-        $content .= "use App\Repositories\\".$model_name."Repository;".PHP_EOL;
-        $content .= "use Thtg88\MmCms\Http\Requests\UpdateRequest;";
-
-        return $content;
-    }
-
-    /**
-     * Returns the constructor for the request from a given model name.
-     *
-     * @param string $model_name
-     * @return string
-     */
-    private function getContentModelRequestConstructor($model_name): string
-    {
-        $content = '';
-        $content .= "class Update".$model_name."Request extends UpdateRequest".PHP_EOL;
-        $content .= "{".PHP_EOL;
-        $content .= "    /**".PHP_EOL;
-        $content .= "     * Create a new request instance.".PHP_EOL;
-        $content .= "     *".PHP_EOL;
-        $content .= "     * @param	\Thtg88\MmCms\Repositories\\".$model_name."Repository	\$repository".PHP_EOL;
-        $content .= "     * @return void".PHP_EOL;
-        $content .= "     */".PHP_EOL;
-        $content .= "    public function __construct(".$model_name."Repository \$repository)".PHP_EOL;
-        $content .= "    {".PHP_EOL;
-        $content .= "        \$this->repository = \$repository;".PHP_EOL;
-        $content .= "    }".PHP_EOL;
-
-        return $content;
-    }
-
-    /**
-     * Returns the authorize method content for the request from a given model name.
-     *
-     * @return string
-     */
-    private function getContentModelRequestAuthorizeMethodContent(): string
-    {
-        $content = '';
-        $content .= "return \$this->authorizeResourceExist();";
-
-        return $content;
+        Artisan::call('make:resource-request', [
+            'name' => $model_name,
+            '--method' => 'update',
+        ]);
     }
 }
