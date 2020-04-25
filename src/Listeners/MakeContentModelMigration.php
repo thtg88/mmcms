85d690a4957e47b3a7c85903b230ebc3777cec8d
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-25 16:08:05 +0100

    Refactored MakeContentModelMigration event listener

--- src/Listeners/MakeContentModelMigration.php
+++ src/Listeners/MakeContentModelMigration.php
@@ -43,42 +43,51 @@ class MakeContentModelMigration
             ->where('migration', 'like', '%'.$migration_name.'%')
             ->first();
 
-        if ($migration === null) {
-            // If migration N/A we make it
-            Artisan::call('make:migration', [
-                'name' => $migration_name,
-                '--create' => $table_name,
-            ]);
-
-            // Then we get the last migration so we can insert our custom fields
-            $migrations = $this->filesystem
-                ->files(Container::getInstance()->databasePath('migrations'));
-
-            if (count($migrations) > 0) {
-                $last_migration = $migrations[count($migrations) - 1].'';
-
-                if (strpos($last_migration, $migration_name) !== false) {
-                    $last_migration_content = file_get_contents($last_migration);
-
-                    if ($last_migration_content !== false) {
-                        $replace_content = '';
-                        $replace_content .= "\$table->timestamp('deleted_at')->nullable();\n";
-                        $replace_content .= "            \$table->timestamp('created_at')->nullable();\n";
-                        $replace_content .= "            \$table->timestamp('updated_at')->nullable();";
-
-                        $last_migration_content = str_replace(
-                            '$table->timestamps();',
-                            $replace_content,
-                            $last_migration_content
-                        );
-
-                        file_put_contents(
-                            $last_migration,
-                            $last_migration_content
-                        );
-                    }
-                }
-            }
+        if ($migration !== null) {
+            return;
         }
+
+        // If migration N/A we make it
+        Artisan::call('make:migration', [
+            'name' => $migration_name,
+            '--create' => $table_name,
+        ]);
+
+        // Then we get the last migration so we can insert our custom fields
+        $migrations = $this->filesystem
+            ->files(Container::getInstance()->databasePath('migrations'));
+
+        if (count($migrations) === 0) {
+            return;
+        }
+
+        // Force it to string by appending an empty one at the end
+        $last_migration = $migrations[count($migrations) - 1].'';
+
+        if (strpos($last_migration, $migration_name) === false) {
+            return;
+        }
+
+        $last_migration_content = file_get_contents($last_migration);
+
+        if ($last_migration_content === false) {
+            return;
+        }
+
+        $replace_content = '';
+        $replace_content .= "\$table->timestamp('deleted_at')->nullable();\n";
+        $replace_content .= "            \$table->timestamp('created_at')->nullable();\n";
+        $replace_content .= "            \$table->timestamp('updated_at')->nullable();";
+
+        $last_migration_content = str_replace(
+            '$table->timestamps();',
+            $replace_content,
+            $last_migration_content
+        );
+
+        file_put_contents(
+            $last_migration,
+            $last_migration_content
+        );
     }
 }
