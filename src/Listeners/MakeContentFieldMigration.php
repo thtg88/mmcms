d00b3fb9ba885da79d57a74eb43c8e5c4202a2e5
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-26 11:02:54 +0100

    Refactored make content field migration event listener

--- src/Listeners/MakeContentFieldMigration.php
+++ src/Listeners/MakeContentFieldMigration.php
@@ -35,7 +35,8 @@ class MakeContentFieldMigration
     public function handle(ContentFieldStored $event)
     {
         $table_name = $event->content_field->content_model->table_name;
-        $migration_name = 'add_'.$event->content_field->name.'_column_to_'.$table_name.'_table';
+        $migration_name = 'add_'.$event->content_field->name.'_column_to_'.
+            $table_name.'_table';
 
         // If migration N/A we make it
         Artisan::call('make:migration', [
@@ -44,47 +45,63 @@ class MakeContentFieldMigration
         ]);
 
         // Then we get the last migration so we can insert our custom fields
-        $migrations = $this->filesystem->files(Container::getInstance()->databasePath('migrations'));
+        $migrations = $this->filesystem
+            ->files(Container::getInstance()->databasePath('migrations'));
 
-        if (count($migrations) > 0) {
-            $last_migration = $migrations[count($migrations) - 1].'';
-
-            if (strpos($last_migration, $migration_name) !== false) {
-                $last_migration_content = file_get_contents($last_migration);
-
-                if ($last_migration_content !== false) {
-                    $search_content = '';
-                    $search_content .= "public function up()\n";
-                    $search_content .= "    {\n";
-                    $search_content .= "        Schema::table('".$table_name."', function (Blueprint \$table) {\n";
-                    $search_content .= "            //";
-
-                    $replace_content = '';
-                    $replace_content .= "public function up()\n";
-                    $replace_content .= "    {\n";
-                    $replace_content .= "         Schema::table('".$table_name."', function (Blueprint \$table) {\n";
-                    $replace_content .= "            \$table->".$event->content_field->content_type->content_migration_method->name;
-                    $replace_content .= "('".$event->content_field->name."')->nullable();";
-
-                    $last_migration_content = str_replace($search_content, $replace_content, $last_migration_content);
-
-                    $search_content = '';
-                    $search_content .= "public function down()\n";
-                    $search_content .= "    {\n";
-                    $search_content .= "        Schema::table('".$table_name."', function (Blueprint \$table) {\n";
-                    $search_content .= "            //";
-
-                    $replace_content = '';
-                    $replace_content .= "public function down()\n";
-                    $replace_content .= "    {\n";
-                    $replace_content .= "         Schema::table('".$table_name."', function (Blueprint \$table) {\n";
-                    $replace_content .= "            \$table->dropColumn('".$event->content_field->name."');";
-
-                    $last_migration_content = str_replace($search_content, $replace_content, $last_migration_content);
-
-                    file_put_contents($last_migration, $last_migration_content);
-                }
-            }
+        if (count($migrations) === 0) {
+            return;
         }
+
+        // Cast to string by appending an empty string
+        $last_migration = $migrations[count($migrations) - 1].'';
+
+        if (strpos($last_migration, $migration_name) === false) {
+            return;
+        }
+
+        $last_migration_content = file_get_contents($last_migration);
+
+        if ($last_migration_content === false) {
+            return;
+        }
+
+        $search_content = '';
+        $search_content .= "public function up()".PHP_EOL;
+        $search_content .= "    {".PHP_EOL;
+        $search_content .= "        Schema::table('".$table_name."', function (Blueprint \$table) {".PHP_EOL;
+        $search_content .= "            //";
+
+        $replace_content = '';
+        $replace_content .= "public function up()".PHP_EOL;
+        $replace_content .= "    {".PHP_EOL;
+        $replace_content .= "         Schema::table('".$table_name."', function (Blueprint \$table) {".PHP_EOL;
+        $replace_content .= "            \$table->".$event->content_field->content_type->content_migration_method->name;
+        $replace_content .= "('".$event->content_field->name."')->nullable();";
+
+        $last_migration_content = str_replace(
+            $search_content,
+            $replace_content,
+            $last_migration_content
+        );
+
+        $search_content = '';
+        $search_content .= "public function down()".PHP_EOL;
+        $search_content .= "    {".PHP_EOL;
+        $search_content .= "        Schema::table('".$table_name."', function (Blueprint \$table) {".PHP_EOL;
+        $search_content .= "            //";
+
+        $replace_content = '';
+        $replace_content .= "public function down()".PHP_EOL;
+        $replace_content .= "    {".PHP_EOL;
+        $replace_content .= "         Schema::table('".$table_name."', function (Blueprint \$table) {".PHP_EOL;
+        $replace_content .= "            \$table->dropColumn('".$event->content_field->name."');";
+
+        $last_migration_content = str_replace(
+            $search_content,
+            $replace_content,
+            $last_migration_content
+        );
+
+        file_put_contents($last_migration, $last_migration_content);
     }
 }
