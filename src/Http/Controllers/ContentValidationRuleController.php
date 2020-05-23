34cb2b0527bbce1bd36f425b1cd87171f0c2ebd1
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-23 11:55:54 +0100

    Added content validation rule search feature

--- src/Http/Controllers/ContentValidationRuleController.php
+++ src/Http/Controllers/ContentValidationRuleController.php
@@ -6,6 +6,7 @@ use Thtg88\MmCms\Http\Requests\ContentValidationRule\DestroyRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\IndexRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\PaginateRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\RestoreRequest;
+use Thtg88\MmCms\Http\Requests\ContentValidationRule\SearchRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\ShowRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\StoreRequest;
 use Thtg88\MmCms\Http\Requests\ContentValidationRule\UpdateRequest;
@@ -13,6 +14,7 @@ use Thtg88\MmCms\Http\Requests\Contracts\DestroyRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\IndexRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\PaginateRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\RestoreRequestInterface;
+use Thtg88\MmCms\Http\Requests\Contracts\SearchRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\ShowRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\StoreRequestInterface;
 use Thtg88\MmCms\Http\Requests\Contracts\UpdateRequestInterface;
@@ -30,6 +32,7 @@ class ContentValidationRuleController extends Controller
         IndexRequestInterface::class => IndexRequest::class,
         PaginateRequestInterface::class => PaginateRequest::class,
         RestoreRequestInterface::class => RestoreRequest::class,
+        SearchRequestInterface::class => SearchRequest::class,
         ShowRequestInterface::class => ShowRequest::class,
         StoreRequestInterface::class => StoreRequest::class,
         UpdateRequestInterface::class => UpdateRequest::class,
---
+++ tests/Feature/ContentValidationRule/Search/WithUrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Thtg88\MmCms\Tests\Feature\ContentValidationRule\Search;
+
+trait WithUrl
+{
+    /**
+     * Return the route to use for these tests from a given parameters array.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string
+    {
+        return route('mmcms.content-validation-rules.search');
+    }
+}
