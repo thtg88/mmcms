27dd49d231f9dc3e4af86fa21a37f2f3b38e1b2c
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-25 22:26:22 +0100

    Added existsWithoutSoftDeleted to base Rule class

--- src/Rules/Rule.php
+++ src/Rules/Rule.php
@@ -3,6 +3,7 @@
 namespace Thtg88\MmCms\Rules;
 
 use Illuminate\Validation\Rule as BaseRule;
+use Thtg88\LaravelExistsWithoutSoftDeletedRule\Rules\ExistsWithoutSoftDeletedRule;
 
 class Rule extends BaseRule
 {
@@ -18,6 +19,20 @@ class Rule extends BaseRule
         string $column = 'NULL'
     ): ExistsCaseInsensitive {
         return new ExistsCaseInsensitive($table, $column);
+    }
+
+    /**
+     * Get a exists without soft deleted constraint builder instance.
+     *
+     * @param string $table
+     * @param string $column
+     * @return \Thtg88\LaravelExistsWithoutSoftDeletedRule\Rules\ExistsWithoutSoftDeletedRule
+     */
+    public static function existsWithoutSoftDeleted(
+        string $table,
+        string $column = 'NULL'
+    ): ExistsWithoutSoftDeletedRule {
+        return new ExistsWithoutSoftDeletedRule($table, $column);
     }
 
     /**
